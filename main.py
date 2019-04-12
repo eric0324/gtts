235b457a0bc2b2e6a1ffbe8cfc45068c3d2a09a1
Eric <smart032410@gmail.com>
2019-04-12 13:33:40 +0800

    Support input lang

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 """Google Cloud Text-To-Speech API sample application .
 Example usage:
-    python main.py -text "艾瑞克好棒棒" -fname "my_file_name"
+    python main.py -text "艾瑞克好棒棒" -fname "my_file_name" -lang "zh-TW"
 """
 import datetime
 from argparse import ArgumentParser
@@ -11,8 +11,8 @@ from gtts import gTTS
 
 
 # [START tts_synthesize_text]
-def synthesize_text(text, name):
-    tts=gTTS(text=text, lang='zh-TW')
+def synthesize_text(text, name, lang):
+    tts=gTTS(text=text, lang=lang)
     tts.save("".join(
             [
                 "output/",
@@ -25,8 +25,9 @@ def synthesize_text(text, name):
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("-text", help="positional argument 1")
-    parser.add_argument("-fname", help="optional argument",)
+    parser.add_argument("-text", help="Input synthesize text", default="你好")
+    parser.add_argument("-fname", help="Input file name", default="hello")
+    parser.add_argument("-lang", help="Input gtts language", default="zh-TW")
     args = parser.parse_args()
 
-    synthesize_text(args.text, args.fname)
+    synthesize_text(args.text, args.fname, args.lang)
